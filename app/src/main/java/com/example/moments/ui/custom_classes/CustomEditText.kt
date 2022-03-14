1dcb3949af47f529991f866757e28cc46f21111c
xmax2807 <19127425@student.hcmus.edu.vn>
2022-03-14 19:25:44 +0700

    update error state

---
+++ app/src/main/java/com/example/moments/ui/custom_classes/CustomEditText.kt
@@ -0,0 +1,19 @@
+package com.example.moments.ui.custom_classes
+
+import android.content.Context
+import android.util.AttributeSet
+import androidx.appcompat.widget.AppCompatEditText
+import com.example.moments.R
+
+class CustomEditText(context:Context, attrs: AttributeSet) : AppCompatEditText(context, attrs) {
+    private val STATE_ERROR = IntArray(1){R.attr.state_error}
+    var isError = false
+
+    override fun onCreateDrawableState(extraSpace: Int): IntArray {
+        val drawableState = super.onCreateDrawableState(extraSpace + 1)
+        if(isError){
+            mergeDrawableStates(drawableState, STATE_ERROR)
+        }
+        return drawableState
+    }
+}
--- app/src/main/java/com/example/moments/ui/login/LoginActivityView.kt
+++ app/src/main/java/com/example/moments/ui/login/LoginActivityView.kt
@@ -4,16 +4,17 @@ package com.example.moments.ui.login
 import android.os.Bundle
 import android.widget.EditText
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.get
 import androidx.core.widget.addTextChangedListener
 import com.example.moments.R
 import com.example.moments.ui.base.BaseActivity
+import com.example.moments.ui.custom_classes.CustomEditText
+import com.google.android.material.textfield.TextInputLayout
 
 
 class LoginActivityView : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
-
-        val a = findViewById<EditText>(R.id.editTextTextPersonName)
     }
 }
