fc004ad17ba921d363f20ed1daa23a14365cb6f9
xmax2807 <19127425@student.hcmus.edu.vn>
2022-03-14 16:10:45 +0700

    sign up page

---
+++ app/src/main/java/com/example/moments/ui/sign_up/SignUpActivityView.kt
@@ -0,0 +1,12 @@
+package com.example.moments.ui.sign_up
+
+import com.example.moments.R
+import android.os.Bundle
+import com.example.moments.ui.base.BaseActivity
+
+class SignUpActivityView : BaseActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_sign_up)
+    }
+}
