70dacb9bc9dd94b91095b5e2d1082458c6e1e9e7
19127324 <64062447+19127324@users.noreply.github.com>
2022-03-14 15:55:11 +0700

    Add Forget Password Step 2,3 Layout

--- app/src/main/java/com/example/moments/ui/forgetPassword/stepOne/ForgetPasswordActivityStepOneView.kt
+++ app/src/main/java/com/example/moments/ui/forgetPassword/stepOne/ForgetPasswordActivityStepOneView.kt
@@ -5,7 +5,7 @@ import android.os.Bundle
 import com.example.moments.R
 import com.example.moments.ui.base.BaseActivity
 
-class ForgetPasswordStepOneView : BaseActivity() {
+class ForgetPasswordActivityStepOneView : BaseActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_forget_password_step_one)
---
+++ app/src/main/java/com/example/moments/ui/forgetPassword/stepTwo/ForgetPasswordActivityStepTwoView.kt
@@ -0,0 +1,14 @@
+package com.example.moments.ui.forgetPassword.stepTwo
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.moments.R
+import com.example.moments.ui.base.BaseActivity
+
+class ForgetPasswordActivityStepTwoView : BaseActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_forget_password_step_two)
+
+    }
+}
