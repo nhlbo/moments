69ea708af3788ccfad8f5b26bd0efdaf11991326
xmax2807 <19127425@student.hcmus.edu.vn>
2022-03-24 12:19:21 +0700

    create post layout and it's component

--- app/src/main/java/com/example/moments/ui/comment/CommentActivityView.kt
+++ app/src/main/java/com/example/moments/ui/comment/CommentActivityView.kt
@@ -2,6 +2,7 @@ package com.example.moments.ui.comment
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.moments.R
 
 class CommentActivityView : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
