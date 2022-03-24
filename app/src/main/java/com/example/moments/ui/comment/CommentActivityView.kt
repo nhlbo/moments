882a0ce0bc4fb63fc23882c57ec7f8939c071552
xmax2807 <19127425@student.hcmus.edu.vn>
2022-03-24 08:35:18 +0700

    create comment layout and minor layout fixes

---
+++ app/src/main/java/com/example/moments/ui/comment/CommentActivityView.kt
@@ -0,0 +1,11 @@
+package com.example.moments.ui.comment
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class CommentActivityView : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_comment)
+    }
+}
