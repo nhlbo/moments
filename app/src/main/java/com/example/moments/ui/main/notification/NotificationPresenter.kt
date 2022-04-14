24a33a8685cee91a01b93dea9c9d39c72d00b5e2
Tran Xuan Son <khatmausr502@gmail.com>
2022-04-14 14:24:51 +0700

    set up fragment search & notification

---
+++ app/src/main/java/com/example/moments/ui/main/notification/NotificationPresenter.kt
@@ -0,0 +1,17 @@
+package com.example.moments.ui.main.notification
+
+import com.example.moments.ui.base.BasePresenter
+import com.example.moments.util.SchedulerProvider
+import io.reactivex.disposables.CompositeDisposable
+import javax.inject.Inject
+
+class NotificationPresenter<V: INotificationView, I: INotificationInteractor> @Inject internal constructor(
+    interactor: I,
+    schedulerProvider: SchedulerProvider,
+    disposable: CompositeDisposable
+) : BasePresenter<V, I>(
+    interactor = interactor,
+    schedulerProvider = schedulerProvider,
+    compositeDisposable = disposable
+), INotificationPresenter<V, I> {
+}
---
+++ app/src/main/java/com/example/moments/ui/main/search/SearchPresenter.kt
@@ -0,0 +1,17 @@
+package com.example.moments.ui.main.search
+
+import com.example.moments.ui.base.BasePresenter
+import com.example.moments.util.SchedulerProvider
+import io.reactivex.disposables.CompositeDisposable
+import javax.inject.Inject
+
+class SearchPresenter<V : ISearchView, I : ISearchInteractor> @Inject internal constructor(
+    interactor: I,
+    schedulerProvider: SchedulerProvider,
+    disposable: CompositeDisposable
+) : BasePresenter<V, I>(
+    interactor = interactor,
+    schedulerProvider = schedulerProvider,
+    compositeDisposable = disposable
+), ISearchPresenter<V, I> {
+}
