90dd2d71414047bffe98e41cbd0ed35475071fc6
Tran Xuan Son <tranxuanson2013@gmail.com>
2022-04-21 11:08:06 +0700

    momment inject

---
+++ app/src/main/java/com/example/moments/ui/main/moments/MommentsFragmentPresenter.kt
@@ -0,0 +1,17 @@
+package com.example.moments.ui.main.moments
+
+import com.example.moments.ui.base.BasePresenter
+import com.example.moments.util.SchedulerProvider
+import io.reactivex.disposables.CompositeDisposable
+import javax.inject.Inject
+
+class MommentsFragmentPresenter<V: IMommentsView, I: IMommentsInteractor>@Inject internal constructor(
+    interactor: I,
+    schedulerProvider: SchedulerProvider,
+    disposable: CompositeDisposable
+) : BasePresenter<V, I>(
+    interactor = interactor,
+    schedulerProvider = schedulerProvider,
+    compositeDisposable = disposable
+), IMommentsPresenter<V,I> {
+}
