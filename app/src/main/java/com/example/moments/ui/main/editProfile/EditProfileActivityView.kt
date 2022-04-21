ae4241c79bb6f16fbbe57e992799d95f08c50c4f
Tran Xuan Son <tranxuanson2013@gmail.com>
2022-04-21 18:32:25 +0700

    newsfeed data set

--- app/src/main/java/com/example/moments/ui/main/editProfile/EditProfileActivityView.kt
+++ app/src/main/java/com/example/moments/ui/main/editProfile/EditProfileActivityView.kt
@@ -3,13 +3,11 @@ package com.example.moments.ui.main.editProfile
 import android.os.Bundle
 import com.example.moments.R
 import com.example.moments.ui.base.BaseActivity
-import com.example.moments.ui.main.EditProfile.IEditProfileActivityInteractor
-import com.example.moments.ui.main.EditProfile.IEditProfileActivityPresenter
-import com.example.moments.ui.main.EditProfile.IEditProfileActivityView
 import kotlinx.android.synthetic.main.activity_edit_profile.*
 import javax.inject.Inject
 
 class EditProfileActivityView : BaseActivity(), IEditProfileActivityView {
+
     @Inject
     lateinit var presenter: IEditProfileActivityPresenter<IEditProfileActivityView, IEditProfileActivityInteractor>
 
--- app/src/main/java/com/example/moments/ui/main/newsFeed/NewsFeedFragmentView.kt
+++ app/src/main/java/com/example/moments/ui/main/newsFeed/NewsFeedFragmentView.kt
@@ -2,17 +2,23 @@ package com.example.moments.ui.main.newsFeed
 
 import android.content.Intent
 import android.os.Bundle
+import android.util.Log
 import android.view.LayoutInflater
+import android.view.MotionEvent
 import android.view.View
 import android.view.ViewGroup
+import androidx.navigation.NavDeepLinkRequest
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 import com.example.moments.R
+import com.example.moments.data.model.RetrievedPost
 import com.example.moments.ui.base.BaseFragment
 import com.example.moments.ui.main.message.MessageActivityView
 import com.google.firebase.firestore.DocumentSnapshot
 import kotlinx.android.synthetic.main.activity_news_feed.*
 import javax.inject.Inject
 
-class NewsFeedFragmentView : BaseFragment(), INewsFeedView {
+class NewsFeedFragmentView : BaseFragment(), INewsFeedView, IAdapterCallBack {
 
     companion object {
         fun newInstance(): NewsFeedFragmentView {
@@ -22,6 +28,9 @@ class NewsFeedFragmentView : BaseFragment(), INewsFeedView {
 
     @Inject
     internal lateinit var presenter: INewsFeedPresenter<INewsFeedView, INewsFeedInteractor>
+
+    @Inject
+    internal lateinit var layoutManager: LinearLayoutManager
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -34,15 +43,43 @@ class NewsFeedFragmentView : BaseFragment(), INewsFeedView {
         presenter.onAttach(this)
         super.onViewCreated(view, savedInstanceState)
         newsfeed_header_bar.inflateMenu(R.menu.header_newsfeeds)
-        setUpOnClicked()
     }
 
     override fun setUp() {
+        layoutManager.orientation = LinearLayoutManager.VERTICAL
+        rcNewsfeedPanel.layoutManager = layoutManager
+        rcNewsfeedPanel.adapter = context?.let { NewsFeedAdapter(it, listPost, this) }
+        rcNewsfeedPanel.isNestedScrollingEnabled = false
+        rcNewsfeedPanel.addOnItemTouchListener(object : RecyclerView.OnItemTouchListener {
+            override fun onInterceptTouchEvent(view: RecyclerView, event: MotionEvent): Boolean {
+                when (event.action) {
+                    MotionEvent.ACTION_DOWN -> rcNewsfeedPanel.parent
+                        ?.requestDisallowInterceptTouchEvent(true)
+                }
+                return false
+            }
+
+            override fun onTouchEvent(rv: RecyclerView, e: MotionEvent) {
+            }
+
+            override fun onRequestDisallowInterceptTouchEvent(disallowIntercept: Boolean) {
+            }
+        })
+        setUpOnClicked()
         presenter.onViewPrepared()
     }
 
-    override fun updatePost(listPost: List<DocumentSnapshot>) {
-        TODO("Not yet implemented")
+    override fun updatePost(listPost: List<RetrievedPost>) {
+        (rcNewsfeedPanel.adapter as NewsFeedAdapter).updatePost(listPost)
+    }
+
+    override fun onItemTouch(position: Int, command: String) {
+        if(command == "showComment"){
+//            val request = NavDeepLinkRequest.Builder
+//                .fromUri(R.string.commentFragment.toString().toUri())
+//                .build()
+//            findNavController().navigate(R.id.commentFragmentView)
+        }
     }
 
     override fun toString(): String = "newsfeedFragment"
@@ -54,7 +91,7 @@ class NewsFeedFragmentView : BaseFragment(), INewsFeedView {
 
     private fun setUpOnClicked() {
         newsfeed_header_bar.setOnMenuItemClickListener {
-            when(it.itemId){
+            when (it.itemId) {
                 R.id.storyBtn -> {
                     true
                 }
@@ -67,4 +104,5 @@ class NewsFeedFragmentView : BaseFragment(), INewsFeedView {
             }
         }
     }
+
 }
