65118ae09f20a74632289099a03d3930c2a52240
xmax2807 <19127425@student.hcmus.edu.vn>
2022-05-01 15:49:52 +0700

    post view

--- app/src/main/java/com/example/moments/ui/main/viewPost/ViewPostActivityView.kt
+++ app/src/main/java/com/example/moments/ui/main/viewPost/ViewPostActivityView.kt
@@ -3,10 +3,24 @@ package com.example.moments.ui.main.viewPost
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.example.moments.R
+import com.example.moments.data.model.RetrievedPost
+import com.example.moments.ui.base.BaseActivity
 
-class ViewPostActivityView : AppCompatActivity() {
+class ViewPostActivityView : BaseActivity(), IViewPostView {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_view_post)
+    }
+
+    override fun onFragmentAttached() {
+        TODO("Not yet implemented")
+    }
+
+    override fun onFragmentDetached(tag: String) {
+        TODO("Not yet implemented")
+    }
+
+    override fun updatePost(post: RetrievedPost) {
+        TODO("Not yet implemented")
     }
 }
