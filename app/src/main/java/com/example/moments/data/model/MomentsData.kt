28a253be1d6f03be00b7f34a5390be272335f85f
xmax2807 <19127425@student.hcmus.edu.vn>
2022-05-05 01:39:06 +0700

    fix layout

--- app/src/main/java/com/example/moments/data/model/MomentsData.kt
+++ app/src/main/java/com/example/moments/data/model/MomentsData.kt
@@ -3,6 +3,7 @@ package com.example.moments.data.model
 class MomentsData(
     val user_id: String,
     val user_ava: String,
+    val username:String,
     val post_id : String,
     val content: String,
     val audio: String,
